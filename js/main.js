8db72408fb9ae839c9e728a9c1fe4348b6f6a9a6
aaronBeryUcl <a.bery@ucl.ac.uk>
2015-04-30 12:03:05 +0100

    update all footers to match listing on ucl homepage

--- js/main.js
+++ js/main.js
@@ -1,5 +1,38 @@
+getassetLocation = function(part){
+	var assetUrl = document.URL;
+	var domainParam = assetUrl.replace(/^([^\?]*)\?(.*)(\&*)domain=([^&]+)(.*)$/ig,'$4');
+	var assetDomain = "//cdn.ucl.ac.uk/";
+	var libLocation = 'indigo/js/lib'
+	if(typeof domainParam!=='undefined'){
+
+		switch(domainParam){
+			case "static":
+				assetDomain = "//static.ucl.ac.uk/";
+			break;
+			case "local":
+				assetDomain = "//static-local/";
+				libLocation = 'indigo/js/src';
+				ext = "";
+			break;
+			case "uat":
+				assetDomain = "//static-uat.ucl.ac.uk/";
+			break;
+		}
+	}
+	if(part==='domain'){
+		return assetDomain;
+	}else{
+		return assetDomain + libLocation;
+	}
+	
+}
+
+var fullAssetLocation = getassetLocation();
+var assetLocation = getassetLocation('domain');
+var urlArgs = "";
+
 require.config({
-	baseUrl: 'js/lib',
+	baseUrl: fullAssetLocation,
 	paths: {
 		app: '../app'
 		,allsite: 'all-site.min'
@@ -7,12 +40,12 @@ require.config({
 		,jquery: globalSiteSpecificVars.pathToJquery
 		,underscore: '//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.6.0/underscore-min'
 		,backbone: '//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.1.2/backbone-min'
-		,fastclick: 'fastclick'
-		,googleAnalyticsLib: 'googleAnalytics.min'
-		,owl: 'owl.carousel.min'
-		,jwplayer: 'jwplayer'
-		,handleBars: 'handlebars.min'
-		,typeAheadBundle: 'typeahead.bundle.min'
+		,fastclick: assetLocation + 'fastclick'
+		,googleAnalyticsLib: assetLocation + 'googleAnalytics.min'
+		,owl: assetLocation + 'owl.carousel.min'
+		,jwplayer: assetLocation + 'jwplayer'
+		,handleBars: assetLocation + 'handlebars.min'
+		,typeAheadBundle: assetLocation + 'typeahead.bundle.min'
 	}
 	,shim:{
 		allsite: {
