29d5becc35384a9e117e10f473a0a126faf796ed
Aaron Bery <aaron@Aarons-MacBook-Pro.local>
2016-04-12 13:55:32 +0100

    update google tracking for call to action links

--- js/src/all-site.js
+++ js/src/all-site.js
@@ -1,106 +1,116 @@
-var _gaq = _gaq || []
-  ,$ = jQuery
-  ,filetypes = /\.(zip|exe|pdf|doc*|xls*|ppt*|mp3|xlsx)$/i
-  ,baseHref = ''
-  ,gaIterator = 0;
+var _gaq = _gaq || [];
 
-if ($('base').attr('href') != undefined)
-  baseHref = $('base').attr('href');
+(function(){
 
-if(globalSiteSpecificVars.googleAnalyticsIdsArray instanceof Array){
-  (function() { 
-    //default UCL Google Analytics tracking
-    globalSiteSpecificVars.googleAnalyticsIdsArray.push('UA-943297-1');
+  var $ = jQuery
+    ,filetypes = /\.(zip|exe|pdf|doc*|xls*|ppt*|mp3|xlsx)$/i
+    ,baseHref = ''
+    ,gaIterator = 0;
 
-    //set up site specif Google Analytics tracking
-    for(gaIterator in globalSiteSpecificVars.googleAnalyticsIdsArray){
-      gaCall(gaIterator,'pageView');
-    }
-    downloadEvents();
+  if ($('base').attr('href') != undefined)
+    baseHref = $('base').attr('href');
 
-    var ga = document.createElement('script');
-    ga.type = 'text/javascript';
-    ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s =document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(ga, s);
-  })();
-}
-function gaCall(gaIterator,callType,customLink,downloadExt,downloadFilePath) {
-  var identifier;
-  if(gaIterator === globalSiteSpecificVars.googleAnalyticsIdsArray.length -1) {
-      identifier = '';
-  } else {
-      identifier = '';
-      //identifier = 't' + gaIterator;
-  }
-  _gaq.push([identifier + '._setAccount', globalSiteSpecificVars.googleAnalyticsIdsArray[gaIterator]]);
-  _gaq.push([identifier + '._setDomainName', 'ucl.ac.uk']);
+  if(globalSiteSpecificVars.googleAnalyticsIdsArray instanceof Array){
+    (function() { 
+      //default UCL Google Analytics tracking
+      globalSiteSpecificVars.googleAnalyticsIdsArray.push('UA-943297-1');
 
-  switch(callType) {
-    case "pageView":
-      _gaq.push(['_setSiteSpeedSampleRate', 100]);
-      _gaq.push([identifier + '._trackPageview']); 
-    break;
-    case "externalLink":
-      _gaq.push([identifier + '_trackEvent', 'External', 'Click', customLink]);
-    break;
-    case "email":
-      _gaq.push([identifier + '_trackEvent', 'Email', 'Click', customLink]);
-    break;
-    case "file":
-      _gaq.push([identifier + '_trackEvent', 'Download', 'Click-' + downloadExt, downloadFilePath]); 
-    break;
-  }
-
-  return;
-}
-function downloadEvents() {
-  var gaIterator = 0;
-
-  $(document).ready(function(){
-    $('a').each(function(e) {
-      var href = $(this).attr('href');
-
-      if (href && (href.match(/^https?\:/i)) && (!href.match(document.domain))) {
-        $(this).click(function(e) {
-          var extLink = href.replace(/^https?\:\/\//i, '');
-
-          for(gaIterator in globalSiteSpecificVars.googleAnalyticsIdsArray){
-            gaCall(gaIterator,'externalLink',extLink);
-          }
-
-          if ($(this).attr('target') != undefined && $(this).attr('target').toLowerCase() != '_blank') {
-            setTimeout(function() { location.href = href; }, 200);
-            return false;
-          }
-        });
-      } else if (href && href.match(/^mailto\:/i)) {
-        $(this).click(function() {
-          var mailLink = href.replace(/^mailto\:/i, '');
-
-          for(gaIterator in globalSiteSpecificVars.googleAnalyticsIdsArray){
-            gaCall(gaIterator,'email',mailLink);
-          }
-        });
-      } else if (href && href.match(filetypes)) {
-        $(this).click(function() {
-          var extension = (/[.]/.exec(href)) ? /[^.]+$/.exec(href) : undefined
-            ,filePath = href;
-
-          for(gaIterator in globalSiteSpecificVars.googleAnalyticsIdsArray){
-            gaCall(gaIterator,'file','',extension,filePath);
-          }
-
-          if ($(this).attr('target') != undefined && $(this).attr('target').toLowerCase() != '_blank') {
-            setTimeout(function() { location.href = href; }, 200);
-            return false;
-          }
-        });
+      //set up site specif Google Analytics tracking
+      for(gaIterator in globalSiteSpecificVars.googleAnalyticsIdsArray){
+        gaCall(gaIterator,'pageView');
       }
+      downloadEvents();
+
+      var ga = document.createElement('script');
+      ga.type = 'text/javascript';
+      ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      var s =document.getElementsByTagName('script')[0];
+      s.parentNode.insertBefore(ga, s);
+    })();
+  }
+  function gaCall(gaIterator,callType,customLink,downloadExt,downloadFilePath) {
+    _gaq.push(['._setAccount', globalSiteSpecificVars.googleAnalyticsIdsArray[gaIterator]]);
+    _gaq.push(['._setDomainName', 'ucl.ac.uk']);
+
+    switch(callType) {
+      case "pageView":
+        _gaq.push(['_setSiteSpeedSampleRate', 100]);
+        _gaq.push(['._trackPageview']); 
+      break;
+      case "cta":
+        _gaq.push(['_trackEvent', 'cta', 'Click', customLink + ' ' + document.URL]);
+      break;
+      case "externalLink":
+        _gaq.push(['_trackEvent', 'External', 'Click', customLink]);
+      break;
+      case "email":
+        _gaq.push(['_trackEvent', 'Email', 'Click', customLink]);
+      break;
+      case "file":
+        _gaq.push(['_trackEvent', 'Download'
+            , 'Click-' + downloadExt, downloadFilePath + ' ' + document.URL]); 
+      break;
+    }
+
+    return;
+  }
+  function downloadEvents() {
+    var gaIterator = 0;
+
+    $(document).ready(function(){
+      $('a').each(function(e) {
+        var href = $(this).attr('href')
+          ,classAttribute = $(this).attr('class');
+
+      if(typeof classAttribute !== 'undefined' && classAttribute.indexOf('cta') > -1) {
+        $(this).click(function() {
+          gaCall(
+            gaIterator
+            ,'cta'
+            ,$(this).html() + ' ' + href 
+          );
+        });
+      } else if (href && (href.match(/^https?\:/i)) && (!href.match(document.domain))) {
+          $(this).click(function(e) {
+            var extLink = href.replace(/^https?\:\/\//i, '');
+
+            for(gaIterator in globalSiteSpecificVars.googleAnalyticsIdsArray){
+              gaCall(gaIterator,'externalLink',extLink);
+            }
+
+            if ($(this).attr('target') != undefined && $(this).attr('target').toLowerCase() != '_blank') {
+              setTimeout(function() { location.href = href; }, 200);
+              return false;
+            }
+          });
+        } else if (href && href.match(/^mailto\:/i)) {
+          $(this).click(function() {
+            var mailLink = href.replace(/^mailto\:/i, '');
+
+            for(gaIterator in globalSiteSpecificVars.googleAnalyticsIdsArray){
+              gaCall(gaIterator,'email',mailLink);
+            }
+          });
+        } else if (href && href.match(filetypes)) {
+          $(this).click(function() {
+            var extension = (/[.]/.exec(href)) ? /[^.]+$/.exec(href) : undefined
+              ,filePath = href;
+
+            for(gaIterator in globalSiteSpecificVars.googleAnalyticsIdsArray){
+              gaCall(gaIterator,'file','',extension,filePath);
+            }
+
+            if ($(this).attr('target') != undefined && $(this).attr('target').toLowerCase() != '_blank') {
+              setTimeout(function() { location.href = href; }, 200);
+              return false;
+            }
+          });
+        }
+      });
     });
-  });
-}
+  }
+})();
 
 // Gridset Overlay JS
 
