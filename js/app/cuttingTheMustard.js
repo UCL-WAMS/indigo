1c018b28db24d488f93fb57c0d0064c53133f07d
benjiebeetle <ben@eveling.com>
2019-10-01 19:13:48 +0100

    Revert cuttingTheMustard.js to original version

--- js/app/cuttingTheMustard.js
+++ js/app/cuttingTheMustard.js
@@ -1,29 +1,20 @@
 var cuttingTheMustard = document.querySelector && window.localStorage && window.addEventListener;
 
 Modernizr.load({
-	// Cutting the mustard as used by the BBC.
+	//cutting the mustard as used by the BBC
 	test : cuttingTheMustard
-	// If old browser load the shiv.
+	//if old browser load the shiv
 	,nope : [
 		'//cdn.ucl.ac.uk/indigo/js/lib/html5shiv-printshiv.min.js'
 		,'//cdn.ucl.ac.uk/indigo/js/lib/respond.min.js'
 		,'//cdn.ucl.ac.uk/indigo/js/lib/respond.proxy.min.js'
 	]
 });
-// Get the path to Drupal JQuery for the site in question.
-if (typeof(Drupal.settings.basePath) != "undefined") {
-	var pathvar = Drupal.settings.basePath;
-} else {
-	var doubles = ["drupal","prospective-students","study"];
-	var tmp = location.pathname.split("/");
-	var pathvar = "/" + (doubles.indexOf(tmp[1]) > -1 && tmp[1].indexOf("drupal-") == -1?tmp[1] + "/" + tmp[2]:tmp[1]) + "/";
-}
-// Set conditional assets for main.js.
+//set conditional assets for main.js
 var globalSiteSpecificVars = {
-//	pathToJquery: '//cdn.ucl.ac.uk/indigo/js/lib/jquery-1.9.1.min',
-	pathToJquery: "https://" + location.hostname + pathvar + "misc/jquery.js?v=1.4.4",
-	googleAnalyticsIdsArray: []//specify array of site specific id's NOT UCL generic UA-943297-1
+	pathToJquery: '//cdn.ucl.ac.uk/indigo/js/lib/jquery-1.9.1.min'
+	,googleAnalyticsIdsArray: []//specify array of site specific id's NOT UCL generic UA-943297-1
 }
-/*if(cuttingTheMustard){
+if(cuttingTheMustard){
 	globalSiteSpecificVars.pathToJquery = '//cdn.ucl.ac.uk/indigo/js/lib/jquery-2.1.1.min';
-}*/
+}
