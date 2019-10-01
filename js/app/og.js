aa823e99fb45bae4c14d580d39353279277d84d4
benjiebeetle <ben@eveling.com>
2019-10-01 19:06:02 +0100

    Add new og app so it can be called through require

---
+++ js/app/og.js
@@ -0,0 +1,18 @@
+require(['jquery'], function($) {
+  $( document ).ready(function() {
+    $('ucl-widget').each(function() {
+      var widget = $(this);
+      var widgetUrl = widget.attr('url');
+      var widgetType = widget.attr('type');
+
+      widget.html('');
+      $.ajax({url: Drupal.settings.basePath + "ucl-widgets?url=" + encodeURIComponent(widgetUrl) + '&type=' + encodeURIComponent(widgetType), success: function(result){
+          $.when(widget.html(result))
+            .then(function(){
+              widget.find('form input[name="LOCAL_BASE"] ').val(widget.data('lssba'));
+              widget.find('form input[name="collection_id"] ').val(widget.data('lsscid'));
+            });
+        }});
+    });
+  });
+});
