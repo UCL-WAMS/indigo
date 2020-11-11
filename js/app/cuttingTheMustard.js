ba64c6b5749b10953ab1932ee953e546d33c3526
benjiebeetle <ben@eveling.com>
2020-11-11 21:30:10 +0000

    Second attempt at JS feed error fix

--- js/app/cuttingTheMustard.js
+++ js/app/cuttingTheMustard.js
@@ -11,10 +11,17 @@ Modernizr.load({
 	]
 });
 //set conditional assets for main.js
+var domainpath = "//cdn.ucl.ac.uk";
+if (location.href.indexOf("cms-dev.ucl.ac.uk") > -1/* || location.href.indexOf("www-pp.ucl.ac.uk") > -1*/) {
+	domainpath = "//static-uat.ucl.ac.uk";
+} else if (location.href.indexOf("local.ucl.ac.uk") > -1) {
+	domainpath = "//static-local.ucl.ac.uk/";
+}
+
 var globalSiteSpecificVars = {
-	pathToJquery: '//cdn.ucl.ac.uk/indigo/js/lib/jquery-1.9.1.min'
+	pathToJquery: domainpath + '/indigo/js/lib/jquery-1.9.1.min'
 	,googleAnalyticsIdsArray: []//specify array of site specific id's NOT UCL generic UA-943297-1
 }
-if(cuttingTheMustard){
-	globalSiteSpecificVars.pathToJquery = '//cdn.ucl.ac.uk/indigo/js/lib/jquery-2.1.1.min';
+if (cuttingTheMustard){
+	globalSiteSpecificVars.pathToJquery = domainpath + '/indigo/js/lib/jquery-2.1.1.min';
 }
