d8d17126cdab0d73125f5a467a5a148ec44b7ae7
benjiebeetle <ben@eveling.com>
2023-12-14 22:15:54 +0000

    Update og.js to prevent timeline Twitter tags from rendering

--- js/app/og.js
+++ js/app/og.js
@@ -6,13 +6,15 @@ require(['jquery'], function($) {
       var widgetType = widget.attr('type');
 
       widget.html('');
-      $.ajax({url: Drupal.settings.basePath + "ucl-widgets?url=" + encodeURIComponent(widgetUrl) + '&type=' + encodeURIComponent(widgetType), success: function(result){
-          $.when(widget.html(result))
-            .then(function(){
-              widget.find('form input[name="LOCAL_BASE"] ').val(widget.data('lssba'));
-              widget.find('form input[name="collection_id"] ').val(widget.data('lsscid'));
-            });
-        }});
+	  if (widgetUrl.indexOf("twitter.com") == -1 || (widgetUrl.indexOf("twitter.com") > -1 && widgetUrl.indexOf("/status/") > -1)) {
+		jQuery.ajax({url: Drupal.settings.basePath + "ucl-widgets?url=" + encodeURIComponent(widgetUrl) + '&type=' + encodeURIComponent(widgetType), success: function(result){
+		  jQuery.when(widget.html(result))
+			.then(function(){
+			  widget.find('form input[name="LOCAL_BASE"] ').val(widget.data('lssba'));
+			  widget.find('form input[name="collection_id"] ').val(widget.data('lsscid'));
+		  });
+		}});
+	  }
     });
   });
 });
